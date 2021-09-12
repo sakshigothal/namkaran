83376c6b3a743f519233cd92f695c1ec34ed9470
sakshigothal <sakshigothal302000@gmail.com>
2021-09-12 13:49:34 +0530

    key change

--- lib/weatherpage.dart
+++ lib/weatherpage.dart
@@ -5,14 +5,13 @@ import 'package:http/http.dart' as http;
 import 'package:namkaran/models/whether_api_data.dart';
 
 class WeatherPage extends StatefulWidget {
-  const WeatherPage({ Key? key }) : super(key: key);
+  const WeatherPage({Key? key}) : super(key: key);
 
   @override
   _WeatherPageState createState() => _WeatherPageState();
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-
   WhetherjsonData? weather;
 
   @override
@@ -21,20 +20,26 @@ class _WeatherPageState extends State<WeatherPage> {
     super.initState();
     callApi();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("${weather?.coord.lat}"),
+        ),
+      ),
     );
   }
 
-
   callApi() async {
-      var resp= await http.get(Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=042a10218a6134f1de90a2dfccfc7b07"));
-      print("${resp.body}");
+    var resp = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=0fe39d45b2232e8803a50768e469ad53"));
+    print("${resp.body}");
 
-      WhetherjsonData weather=WhetherjsonData.fromJson(jsonDecode(resp.body));
-      print("${weather.coord.lat}");
-
+    setState(() {
+      weather = WhetherjsonData.fromJson(jsonDecode(resp.body));
+    });
+    print("${weather?.coord.lat}");
   }
 }
