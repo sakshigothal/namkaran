e1d8cb13096094dced7b62d59993057258954537
sakshigothal <sakshigothal302000@gmail.com>
2021-09-14 13:23:09 +0530

    namkarn only setstate error

--- lib/listpage.dart
+++ lib/listpage.dart
@@ -2,9 +2,15 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:namkaran/common.dart';
 import 'package:namkaran/models/hindu_cat_response.dart';
 
+import 'namemeaning.dart';
+
 class HinduListPage extends StatefulWidget {
+  // final List<HinduCatResponse>? personArr;
+  // final int indexNo;
+  // final HinduCatResponse? selectedPerson;
   const HinduListPage({Key? key}) : super(key: key);
 
   @override
@@ -12,22 +18,9 @@ class HinduListPage extends StatefulWidget {
 }
 
 class _HinduListPageState extends State<HinduListPage> {
-  HinduCatResponse? hcr;
-  List<HinduCatResponse> hinduresp=[];
-  List hindu=[];
-
-  callApi() async {
-    var resp = await http.get(
-      Uri.parse(
-          "http://mapi.trycatchtech.com/v1/naamkaran/post_list_by_cat_and_gender?category_id=8&gender=1"),
-    );
-    print(resp.body);
-    // List response=jsonDecode(resp.body);
-     setState(() {
-       hcr = HinduCatResponse.fromJson(json.decode(resp.body));
-     });
-    print(hcr);
-  }
+  // HinduCatResponse? hcr;
+  List<HinduCatResponse> personArr = [];
+  List respArr = [];
 
   @override
   void initState() {
@@ -36,14 +29,58 @@ class _HinduListPageState extends State<HinduListPage> {
     callApi();
   }
 
+  callApi() async {
+    var resp = await http.get(
+      Uri.parse(
+          "http://mapi.trycatchtech.com/v1/naamkaran/post_list_by_cat_and_gender?category_id=${Common.categoryId}Id&gender=${Common.gender}"),
+    );
+    print(resp.body);
+    respArr = jsonDecode(resp.body);
+
+    for (var item in respArr) {
+      personArr.add(HinduCatResponse.fromJson(item));
+    }
+    // setState(() {
+    //   hcr = HinduCatResponse.fromJson(json.decode(resp.body));
+    // });
+    // print(hcr);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.blue,
-        child: Center(child: Text("${hcr?.meaning}")),
-        // Text("${hcr!.meaning}")
-      ),
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.pink[200],
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  personArr[index].name,
+                  // style: TextStyle(fontSize: 25),
+                ),
+                trailing: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (ctx) => HinduCard(
+                                    personArr: personArr,
+                                    // indexNo: index,
+                                    selectedPerson: personArr[index],
+                                  )));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.pink)),
+                    child: Text("Meaning")),
+              );
+            },
+            itemCount: personArr.length,
+          )
+          // Text("${hcr!.meaning}")
+          ),
     );
   }
 }
